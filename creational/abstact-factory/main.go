c806d7d8548b5137a6a0ab072182d80d350b4e6f
Chhaya Kumar Das <chayakudas.das56@gmail.com>
2022-05-15 16:53:38 +0530

    Abstract Factory Pattern

---
+++ creational/abstact-factory/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	adidasFactory,_ := getSportsFactory("adidas")
+	nikeFactory,_ := getSportsFactory("nike")
+
+	adidas_shoe := adidasFactory.makeShoe()
+	adidas_shirt := adidasFactory.makeShirt()
+
+	nike_shoe := nikeFactory.makeShoe()
+	nike_shirt := nikeFactory.makeShirt()
+
+	printShirtDetails(adidas_shirt)
+	printShirtDetails(nike_shirt)
+
+	printShoeDetails(adidas_shoe)
+	printShoeDetails(nike_shoe)
+}
+
+func printShoeDetails(s iShoe) {
+	fmt.Println("Shoe Logo:",s.getLogo())
+	fmt.Println("Shoe Size:",s.getSize())
+}
+
+func printShirtDetails(s iShirt) {
+	fmt.Println("Shirt Logo:",s.getLogo())
+	fmt.Println("Shirt Size:",s.getSize())
+}
